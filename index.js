2c6fde4d3f72b2c011712ac096d4035b099c4995
Ali Suiarov <alizhansuiarov@gmail.com>
2022-07-11 11:56:40 +0300

    finish project

---
+++ index.js
@@ -0,0 +1,40 @@
+var name = prompt('Введите свое имя!')
+console.log('Ваша имя');
+console.log(name);
+
+var age = prompt('Введите свой возвраст!')
+console.log('Ваш возвраст');
+console.log(age);
+
+var yourAnswer = 'Ваш ответь = '
+var value = 15+20
+var correctAnswer = '; правильный ответь = ' + (value) 
+var string = 'Сколько будет 15 + 20 ? '
+var res = prompt('Сколько будет 15 + 20 = ?')
+console.log(string);
+console.log( yourAnswer + res + correctAnswer );
+
+var value = 10 * 10
+var correctAnswer = '; правильный ответь = ' + (value) 
+var string = 'Сколько будет 10 * 10 ? '
+var res = prompt('Сколько будет 10 * 10 = ?')
+console.log(string);
+console.log( yourAnswer + res + correctAnswer );
+
+var value = 10 / 2
+var correctAnswer = '; правильный ответь = ' + (value)
+var string = 'Сколько будет 10 / 2 ? '
+var res = prompt('Сколько будет 10 / 2 = ?')
+console.log(string);
+console.log( yourAnswer + res + correctAnswer );
+
+var value = 100 % 3
+var correctAnswer = '; правильный ответь = ' + (value)
+var string = 'Сколько будет 100 % 3 ? '
+var res = prompt('Сколько будет 100 % 3 = ?')
+console.log(string);
+console.log( yourAnswer + res + correctAnswer );
+
+alert('Перейдите в консоль разработчика чтобы увидеть правильные ответы!')
+
+
